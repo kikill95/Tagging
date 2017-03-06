9c22de2eb65ae8acf073454af0ceb1b37de5a392
Light <kirill.gusyatin@ekreative.com>
2017-03-06 17:27:06 +0200

    Make an example with image and some tags, delete functionality

--- main.js
+++ main.js
@@ -1,11 +1,70 @@
 'use strict';
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { render } from 'react-dom';
 
 import Tagging from './source/index';
 
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      maxLength: 20,
+      tags: [
+        {
+          title: 'Lorem Ipsum',
+          x: 200,
+          y: 200,
+          index: 0
+        },
+        {
+          title: 'Lorem',
+          x: 200,
+          y: 300,
+          index: 1
+        },
+        {
+          title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          x: 100,
+          y: 100,
+          index: 1
+        }
+      ]
+    };
+
+    this.onReplace = this.onReplace.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+
+  onReplace(element, index, array) {
+    if (array) {
+      this.setState({tags: array});
+    }
+  }
+
+  onDelete(element, index, array) {
+    if (array) {
+      this.setState({tags: array});
+    }
+  }
+
+  render() {
+    return (
+      <Tagging
+        image={'./image.jpg'}
+        width={'512px'}
+        height={'512px'}
+        tags={this.state.tags}
+        onReplace={this.onReplace}
+        onDelete={this.onDelete}
+        maxLength={this.state.maxLength}
+      />
+    );
+  }
+}
+
 render(
-    <Tagging/>,
+    <App/>,
     document.getElementById('react-app')
 );
--- source/index.js
+++ source/index.js
@@ -6,20 +6,90 @@ import './index.scss';
 
 export default class Tagging extends PureComponent {
   static defaultProps = {
-
+    image: '',
+    width: '400px',
+    height: '400px',
+    tags: [],
+    onReplace: Function.prototype,
+    onDelete: Function.prototype,
+    maxLength: 16
   };
 
   static propTypes = {
-
+    image: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.shape({
+        title: PropTypes.string,
+        x: PropTypes.number,
+        y: PropTypes.number,
+        index: PropTypes.number
+    })),
+    onReplace: PropTypes.func,
+    onDelete: PropTypes.func,
+    maxLength: PropTypes.number
   };
 
   constructor(props) {
       super(props);
+
+      this.state = {
+        chosenIndex: null
+      };
+
+      this.deleteTag = this.deleteTag.bind(this);
+      this.replaceTag = this.replaceTag.bind(this);
+  }
+
+  deleteTag(tag, index) {
+    this.setState({chosenIndex: null});
+    this.props.onDelete(tag, index, this.props.tags.filter((tag, ind) => ind !== index));
   }
 
   render() {
       return (
-        <div>Hello World</div>
+        <div
+          className={'tagging-wrapper'}
+          style={{
+            backgroundImage: `url(${this.props.image})`,
+            width: this.props.width,
+            height: this.props.height,
+          }}
+          onClick={() => {
+            this.setState({chosenIndex: null});
+          }}
+        >
+          {this.props.tags.map((tag, index) => {
+            return (
+              <div
+                className={'tagging-element'}
+                key={index}
+                style={{
+                  zIndex: tag.index,
+                  left: tag.x,
+                  top: tag.y
+                }}
+                onClick={event => {
+                  event.stopPropagation();
+                  this.setState({chosenIndex: index});
+                }}
+              >
+                <span className={'title'}>{tag.title.length + 3 > this.props.maxLength ? tag.title.slice(0, this.props.maxLength - 3) + '...' : tag.title}</span>
+                {
+                  this.state.chosenIndex === index ? (
+                    <span
+                      className={'delete'}
+                      onClick={event => {
+                        event.stopPropagation();
+                        this.deleteTag(tag, index);
+                      }}
+                    >X</span>
+                  ) : null
+                }
+              </div>
+            );
+          })}
+        </div>
       );
   }
 }
